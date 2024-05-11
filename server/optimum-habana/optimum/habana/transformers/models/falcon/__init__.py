0075ad2ee7c6d154862cb6400137adda91cc3cbb
Bjorn Runaker <bjorn@runaker.se>
2024-05-11 13:11:27 +0200

    add optimum habana transformers

---
+++ server/optimum-habana/optimum/habana/transformers/models/falcon/__init__.py
@@ -0,0 +1,9 @@
+from .modeling_falcon import (
+    GaudiFalconAttention,
+    GaudiFalconDecoderLayer,
+    GaudiFalconForCausalLM,
+    GaudiFalconMLP,
+    GaudiFalconModel,
+    gaudi_falcon_attention_split_heads,
+    gaudi_falcon_linear_forward,
+)
